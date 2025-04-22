37152848ce8da75e42879eb0284fceb0f5493b8b
yasminhosam <hyassmen750@gmail.com>
2025-04-22 16:33:57 +0200

    Latest version of Home screen without database

--- app/src/main/java/com/example/habit_compose/MainActivity.kt
+++ app/src/main/java/com/example/habit_compose/MainActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,7 +21,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.twotone.Notifications
@@ -33,15 +41,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.habit_compose.CalenderData
-import com.example.habit_compose.NavScreen
+import androidx.navigation.compose.rememberNavController
 import com.example.habit_compose.ui.theme.Habit_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,15 +62,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Habit_composeTheme {
+                val navController = rememberNavController()
+                HabitCategoryScreen(navController = navController)
                 NavScreen()
+
             }
         }
 
     }
 }
-
-
-
 
 
 @Composable
@@ -69,13 +81,11 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
     val selectedDate = rememberSaveable { mutableStateOf(today) }
     val displayedDate = selectedDate.value ?: today
-
+    modifier.background(Color(0xFF29CDE8))
     Column() {
         HeadIcons()
 
-        LazyRow(
-            modifier.background(Color(0xFFE5E8E5))
-        ) {
+        LazyRow() {
             items(items = weekDates) { date ->
                 DateBar(
                     day = date.day,
@@ -86,44 +96,8 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             }
             //when the user select date display its habits otherwise habits of today
         }
-        val habitList = listOf(
-            "habit 1",
-            "habit 2",
-            "habit 3",
-            "habit 4",
-            "habit 5",
-            "habit 6",
-            "habit 7",
-            "habit 8"
-        )
-        Column(
-            modifier
-                .fillMaxSize()
-                .padding(32.dp)
-        ) {
 
-            Text(text = "Habits", fontWeight = FontWeight.Bold, fontSize = 20.sp
-            )
-            LazyColumn(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.padding(top = 22.dp)
-
-            ) {
-                items(habitList) { item ->
-                    Text(
-                        text = item,
-                        style = MaterialTheme.typography.bodyLarge,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(vertical = 10.dp)
-
-                    )
-
-
-                }
-            }
-        }
+        HabitList()
     }
 }
 
@@ -142,16 +116,6 @@ fun HeadIcons() {
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            IconButton(onClick = {/*to calender */ }) {
-                Icon(
-                    imageVector = Icons.Default.DateRange,
-                    contentDescription = "date",
-                    modifier = Modifier.size(28.dp),
-                    tint = Color.Gray,
-
-                    )
-
-            }
             IconButton(onClick = {/*to notification screen */ }) {
 
                 Icon(
@@ -161,6 +125,7 @@ fun HeadIcons() {
                     tint = Color.Gray,
                 )
             }
+
 
         }
         Text(
@@ -184,45 +149,141 @@ fun DateBar(day: String, date: String, isSelected: Boolean, onClick: () -> Unit)
     Card(
         onClick = onClick,
         modifier = Modifier
-            .padding(vertical = 3.dp, horizontal = 12.dp),
-        border = if (isSelected) BorderStroke(
-            2.dp,
-            Color(0xFF449144)
-        ) else null,
+            .padding(vertical = 3.dp, horizontal = 14.dp)
+            .clip(RoundedCornerShape(22.dp)),
         colors = CardDefaults.cardColors(
-            containerColor = Color.Transparent,
+            containerColor = if (isSelected) Color.Black else Color(0xFFF6FEFF)
         )
     )
     {
         Column(
             modifier = Modifier
-                .width(44.dp)
-                .height(48.dp)
+                .width(50.dp)
+                .height(68.dp)
                 .padding(4.dp)
+
         ) {
-            Text(
-                text = date,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                style = MaterialTheme.typography.bodyMedium,
-                color = if (isSelected) Color(0xFF449144)
-                else Color.Black
-            )
-            Text(
-                text = day,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                style = MaterialTheme.typography.bodySmall,
-                color = if (isSelected) Color(0xFF449144)
-                else Color.Black
-            )
+            Box(
+                modifier = Modifier
+                    .size(27.dp)
+                    .shadow(
+                        elevation = 8.dp,
+                        shape = RoundedCornerShape(16.dp), clip = true
+                    )
+                    .clip(CircleShape)
+                    .align(Alignment.CenterHorizontally)
+
+                    .background(Color(0xFFFDFDFD))
+
+            ) {
+
+                Text(
+                    text = date,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(2.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+
+                    )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 9.dp)
+
+            ) {
+                Text(
+                    text = day,
+                    modifier = Modifier.align(Alignment.Center),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color(0xFF55C9D7)
+
+                )
+
+
+            }
 
         }
     }
 }
 
-
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HeadIconsPreview() {
-    HomeScreen()
+fun HabitList() {
 
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .padding(top=5.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .shadow(22.dp, RoundedCornerShape(20.dp), clip = false)
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
+        if(habitCategories.isEmpty()){
+            Column(
+
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(all = 8.dp)
+                    .fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.lifestyle),
+                    contentDescription = "image life style ",
+                    modifier = Modifier
+                        .size(120.dp)
+                        .padding(8.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "You don't have any habits",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Black,
+                    modifier = Modifier.padding(top = 16.dp)
+
+                )
+                Text(
+                    text = "Let's add new habit",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(top = 9.dp)
+                )
+            }
+        }else{
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text("Habits List", fontSize = 23.sp)
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement =  Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(top = 20.dp, bottom = 20.dp),
+
+
+                    ) {
+                    itemsIndexed(habitCategories) { index, habit ->
+                        HabitCardStyled(habit = habit, index = index) {
+
+                        }
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+
+}
+
+@Composable
+fun HabitCardGridStyle(){}
+
+@Preview(showBackground = true, showSystemUi = true, device = "spec:width=411dp,height=891dp")
+@Composable
+fun HomeScreenPreview() {
+
+    NavScreen()
 }
